25826d893fc577a20e3375b70d3094839ca89e52
Kirill Müller <krlmlr+r@mailbox.org>
2023-04-08 12:56:34 +0200

    hash_table()

--- src/code.cpp
+++ src/code.cpp
@@ -1,5 +1,21 @@
+// #define R_NO_REMAP
+// #define STRICT_R_HEADERS
+// #include <Rinternals.h>
+
 #include <cpp11.hpp>
 using namespace cpp11;
 
+extern SEXP R_StringHash;
+
 [[cpp11::register]]
-void fun() {}
+cpp11::strings hash_table() {
+  cpp11::writable::strings out;
+
+  for (size_t i = 0; i < LENGTH(R_StringHash); ++i) {
+    for (SEXP chain = VECTOR_ELT(R_StringHash, i); chain != R_NilValue; chain = ATTRIB(chain)) {
+      out.push_back(chain);
+    }
+  }
+
+  return out;
+}
